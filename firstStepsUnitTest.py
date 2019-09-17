91f6540f0fcb0e89fda409591d696762120ec2cc
Gastón Ríos <gaston.a.rios@gmail.com>
2019-09-17 14:52:16 -0300

    Update firstStepsUnitTest.py

--- firstStepsUnitTest.py
+++ firstStepsUnitTest.py
@@ -16,7 +16,7 @@ class TwitterClass(unittest.TestCase):
         self.user.send_keys("gastonarios@yahoo.com.ar")
 
         self.password = self.driver.find_element_by_name("session[password]")
-        self.password.send_keys("cancu233231685")
+        self.password.send_keys("")
         self.loginButton = self.driver.find_element_by_xpath(
             "/html/body[@class='three-col logged-out ms-windows static-logged-out-home-page no-nav-banners']/div[@id='doc']/div[@class='StaticLoggedOutHomePage']/div[@class='StaticLoggedOutHomePage-content']/div[@class='StaticLoggedOutHomePage-cell StaticLoggedOutHomePage-utilityBlock']/div[@class='StaticLoggedOutHomePage-login']/form[@class='LoginForm js-front-signin']/input[@class='EdgeButton EdgeButton--secondary EdgeButton--medium submit js-submit']")
         self.loginButton.click()
